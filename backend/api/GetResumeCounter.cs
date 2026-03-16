28b64336bec3994e01084f36da7793966d284b58
Drew Davis <davisdre@hotmail.com>
2026-03-16 01:20:42 +0000

    🛠️ Refactor GetResumeCounter function to use Azure Functions Worker model and support multiple outputs

--- backend/api/GetResumeCounter.cs
+++ backend/api/GetResumeCounter.cs
@@ -1,38 +1,48 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.Azure.WebJobs.Extensions.CosmosDB;
-using Microsoft.AspNetCore.Http;
+using System.Net;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using System.Net.Http;
-using System.Text;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 
 namespace Company.Function
 {
-    public static class GetResumeCounter
+    public class GetResumeCounter
     {
-        [FunctionName("GetResumeCounter")]
-        public static HttpResponseMessage Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            [CosmosDB(databaseName:"AzureResume", containerName: "Counter", Connection = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] Counter counter,
-            [CosmosDB(databaseName:"AzureResume", containerName: "Counter", Connection = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] out Counter updatedCounter,
-            ILogger log)
+        private readonly ILogger _logger;
+
+        public GetResumeCounter(ILoggerFactory loggerFactory)
         {
-            // Here is where the counter gets updated
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            _logger = loggerFactory.CreateLogger<GetResumeCounter>();
+        }
 
-            updatedCounter = counter;
-            updatedCounter.Count += 1;
+        [Function("GetResumeCounter")]
+        public MultiResponse Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
+            [CosmosDBInput(databaseName: "AzureResume", containerName: "Counter", Connection = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] Counter counter)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
 
-            var jsonToReturn = JsonConvert.SerializeObject(counter);
-            return new HttpResponseMessage(System.Net.HttpStatusCode.OK)
+            counter.Count += 1;
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "application/json; charset=utf-8");
+            response.WriteString(JsonConvert.SerializeObject(counter));
+
+            // Return both the HTTP response and the document to update CosmosDB
+            return new MultiResponse
             {
-                Content = new StringContent(jsonToReturn, Encoding.UTF8, "application/json")
+                Document = counter,
+                HttpResponse = response
             };
         }
+    }
+
+    // Class to handle multiple outputs (HTTP Response + CosmosDB Document Update)
+    public class MultiResponse
+    {
+        [CosmosDBOutput(databaseName: "AzureResume", containerName: "Counter", Connection = "AzureResumeConnectionString", CreateIfNotExists = false)]
+        public Counter Document { get; set; }
+
+        public HttpResponseData HttpResponse { get; set; }
     }
 }
